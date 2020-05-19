cf8e2cb386c68ca5daf07fad75b973f0fdd0df9b
Ayan Maity <ayan.maity@live.com>
2020-05-19 13:55:49 +0530

    Scrolling List rather than pagination

--- src/app/search-list/search-list.component.ts
+++ src/app/search-list/search-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, Optional } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, Optional, HostListener } from '@angular/core';
 import { MoviedataService } from '../moviedata.service';
 import { MovieList } from '../movie-list';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-search-list',
@@ -14,8 +15,16 @@ export class SearchListComponent implements OnInit, OnChanges {
   spinner:Boolean;
   movieList: MovieList;
   paginationLength:number;
+  pageNo:number;
+  @HostListener('window:scroll') onScroll(){
+    if(window.innerHeight+window.scrollY >= document.documentElement.scrollHeight-10){
+      if(this.paginationLength > this.pageNo && this.movieList.Response=='True'){
+        this.getPageList(this.pageNo+1)
+      }
+    }
+  }
 
-  constructor(@Optional() private moviedataService:MoviedataService) { }
+  constructor(@Optional() private moviedataService:MoviedataService, private snackbar:MatSnackBar) { }
 
   ngOnInit(): void {
   }
@@ -32,26 +41,35 @@ export class SearchListComponent implements OnInit, OnChanges {
       .pipe(
       )
       .subscribe(
-        next=> {this.movieList = next;setTimeout(()=>{this.setPaginator();this.spinner=false;},1000)},
+        next=> {this.movieList = next;this.pageNo = 1;setTimeout(()=>{this.setPaginator();this.spinner=false;},1000)},
         error=> { this.movieList = {'Response':"False","Error":error.statusText}; }
       )
     }
   }
 
   setPaginator(){
-    // this.paginationLength = (this.movieList.totalResults && parseInt(this.movieList.totalResults)%10!==0)? (parseInt(this.movieList.totalResults)/10)+1:(parseInt(this.movieList.totalResults)/10);
-    this.paginationLength = (this.movieList.totalResults)? (parseInt(this.movieList.totalResults)):undefined;
+    this.paginationLength = (this.movieList.totalResults && parseInt(this.movieList.totalResults)%10!==0)? (parseInt(this.movieList.totalResults)/10)+1:(parseInt(this.movieList.totalResults)/10);
   }
 
-  getPageList(e) {
+  getPageList(pageNo) {
     this.spinner = true;
-    this.movieList = undefined;
-    this.moviedataService.getList(this.moviedataService.title,this.moviedataService.type,this.moviedataService.year,e.pageIndex+1)
+    this.moviedataService.getList(this.moviedataService.title,this.moviedataService.type,this.moviedataService.year,pageNo)
     .pipe(
     )
     .subscribe(
-      next=> {this.movieList = next;setTimeout(()=>{this.spinner=false;},1000)},
-      error=> { this.movieList = {'Response':"False","Error":error.statusText}; setTimeout(()=>{this.setPaginator();this.spinner=false;},1000)}
+      next=> {
+        if(next.Response=="True"){
+          this.movieList.Search = this.movieList.Search.concat(next.Search);
+          this.pageNo++;
+        } else {
+          this.snackbar.open(next.Error,'',{duration:2000});
+        }
+        setTimeout(()=>{this.spinner=false;},1000)
+      },
+      error=> { 
+        this.snackbar.open(error.statusText,'',{duration:2000});
+        setTimeout(()=>{this.spinner=false;},1000)
+      }
     )
   }
 
